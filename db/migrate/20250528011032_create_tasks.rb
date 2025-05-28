b8c2be071c6d86e863887c2c14057cc935a01718
Heitor Wick <heitorwick@gmail.com>
2025-05-27 22:43:50 -0300

    start to-do-list

---
+++ db/migrate/20250528011032_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :description
+      t.date :due_date
+      t.boolean :done,  default: false
+
+      t.timestamps
+    end
+  end
+end
