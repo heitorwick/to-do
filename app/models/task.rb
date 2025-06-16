9e7e3f12e65b7e84fb7bf2cf81cfac231c40a244
Heitor Wick <heitorwick@gmail.com>
2025-06-15 22:22:52 -0300

    Adiciona autenticação de usuários com Devise, implementa funcionalidades de busca, filtro e ordenação de tarefas, além de melhorias na interface e na estrutura do banco de dados.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :description, presence: true
   validates :done, inclusion: { in: [true, false] }
+
+  validate :due_date_is_date
+
+  def due_date_is_date
+    if due_date.present? && !due_date.is_a?(Date)
+      errors.add(:due_date, 'deve ser uma data válida')
+    end
+  end
 end
